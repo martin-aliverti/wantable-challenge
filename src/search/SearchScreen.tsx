772e3a5d1dbb67f5cf897fb8f90fff1152433e75
martin aliverti <martin.aliverti@gmail.com>
2020-07-19 13:22:13 -0300

    Search screen displays basic artists data

--- src/search/SearchScreen.tsx
+++ src/search/SearchScreen.tsx
@@ -1,8 +1,37 @@
-import React from "react";
-import { Typography } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { Typography, TextField } from "@material-ui/core";
+import ArtistService, { Artist as ArtistType } from "../services/ArtistService";
 
-export default () => (
-  <div>
-    <Typography>Search for your favourite artists!</Typography>
-  </div>
-);
+export default () => {
+  const [artists, setArtists] = useState<ArtistType[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleChange = (event: any) => {
+    setSearchQuery(event.target.value);
+  };
+
+  useEffect(() => {
+    searchQuery.length && ArtistService.search(searchQuery).then(setArtists);
+  }, [searchQuery]);
+
+  return (
+    <div style={styles.container}>
+      <Typography>Search for your favourite artists!</Typography>
+      <TextField
+        placeholder="Artist name"
+        onChange={handleChange}
+        value={searchQuery}
+      />
+      {artists.map((artist) => (
+        <div>{artist.name}</div>
+      ))}
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    width: 500,
+    padding: 20,
+  },
+};
