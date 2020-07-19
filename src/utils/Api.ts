e08f34925b2ba3c590d8c54ce18c13330582207c
martin aliverti <martin.aliverti@gmail.com>
2020-07-19 13:56:53 -0300

    Moved api url and key to env file

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -1,12 +1,9 @@
-const API_URL = "https://ws.audioscrobbler.com/2.0";
-const standardQuearyString =
-  "api_key=7b46939b16b80d20a18f9eaa1166a9e7&format=json";
+const apiUrl = process.env.REACT_APP_API_URL;
+const standardQuearyString = `api_key=${process.env.REACT_APP_API_KEY}&format=json`;
 
 export default {
   get: async (query: string) => {
-    const response = await fetch(
-      `${API_URL}/?${query}&${standardQuearyString}`
-    );
+    const response = await fetch(`${apiUrl}/?${query}&${standardQuearyString}`);
     return response.json();
   },
 };
